00e0bd49aba7c05a9c84f262b757766b25a2726c
wanghao <117663444@qq.com>
2019-08-26 11:53:13 +0800

    generate antd component

--- src/app/pages/demo/demo-routing.module.ts
+++ src/app/pages/demo/demo-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DemoComponent } from './demo.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DemoComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
--- src/app/pages/demo/demo.module.ts
+++ src/app/pages/demo/demo.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DemoRoutingModule } from './demo-routing.module';
 import { DemoComponent } from './demo.component';
-
+import { LoginComponent } from './login/login.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [DemoComponent],
+  declarations: [DemoComponent, LoginComponent],
   imports: [
     CommonModule,
-    DemoRoutingModule
-  ]
+    DemoRoutingModule,
+    NgZorroAntdModule,
+    ReactiveFormsModule,
+  ],
 })
-export class DemoModule { }
+export class DemoModule {}
