46f736f93951c5fc29f2fb0d17cd729ade61137b
wanghao <117663444@qq.com>
2019-08-26 11:48:22 +0800

    create a demo module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('./pages/welcome/welcome.module').then(m => m.WelcomeModule),
+  },
+  {
+    path: 'demo',
+    loadChildren: () =>
+      import('./pages/demo/demo.module').then(m => m.DemoModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
